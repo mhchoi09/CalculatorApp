ea3bf4489efd9280aad930cf18d24a8283992fe0
Ru <32935256+mhchoi09@users.noreply.github.com>
2025-06-25 10:03:12 +0800

    Initial Commit

---
+++ SampleCalculator.ConsolApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using SampleCalculator.Services;
+
+
+Console.WriteLine("Welcome to the Sample Calculator App!");
+Console.WriteLine("Please enter the first number:");
+var firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the second number:");
+var secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please choose an operation: +, -, *, /");
+string operation = Console.ReadLine();
+switch (operation)
+{
+    case "+":
+        Console.WriteLine($"Result: {Operations.Add(firstNumber, secondNumber)}");
+        break;
+    case "-":
+        Console.WriteLine($"Result: {Operations.Subtract(firstNumber, secondNumber)}");
+        break;
+    case "*":
+        Console.WriteLine($"Result: {Operations.Multiply(firstNumber, secondNumber)}");
+        break;
+    case "/":
+        try
+        {
+            Console.WriteLine($"Result: {Operations.Divide(firstNumber, secondNumber)}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        break;
+    default:
+        Console.WriteLine("Invalid operation selected.");
+        break;
+}   
---
+++ SampleCalculator.Services/Operations.cs
@@ -0,0 +1,32 @@
+﻿namespace SampleCalculator.Services;
+
+public static class Operations
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Divide(int a, int b)
+    {
+        try
+        {
+            var result = a / b;
+
+            return result;
+        }
+        catch (DivideByZeroException)
+        {
+            throw new DivideByZeroException("Cannot divide by zero.");
+        }
+    }
+}
