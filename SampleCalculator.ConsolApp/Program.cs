232bb98ec23faaa4ce6f5d91bc88c10183269652
mhchoi09 <rhui21@live.com.ph>
2025-08-05 01:39:02 +0800

    Add namespace on console app

--- SampleCalculator.ConsolApp/Program.cs
+++ SampleCalculator.ConsolApp/Program.cs
@@ -1,35 +1,43 @@
 ﻿using SampleCalculator.Services;
 
-
-Console.WriteLine("Welcome to the Sample Calculator App!");
-Console.WriteLine("Please enter the first number:");
-var firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Please enter the second number:");
-var secondNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Please choose an operation: +, -, *, /");
-string operation = Console.ReadLine();
-switch (operation)
+namespace SampleCalculator.ConsolApp
 {
-    case "+":
-        Console.WriteLine($"Result: {Operations.Add(firstNumber, secondNumber)}");
-        break;
-    case "-":
-        Console.WriteLine($"Result: {Operations.Subtract(firstNumber, secondNumber)}");
-        break;
-    case "*":
-        Console.WriteLine($"Result: {Operations.Multiply(firstNumber, secondNumber)}");
-        break;
-    case "/":
-        try
+    public static class Program
+    {
+        public static void Main(string[] args)
         {
-            Console.WriteLine($"Result: {Operations.Divide(firstNumber, secondNumber)}");
+            Console.WriteLine("Welcome to the Sample Calculator App!");
+            Console.WriteLine("Please enter the first number:");
+            var firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            var secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please choose an operation: +, -, *, /");
+            string operation = Console.ReadLine() ?? string.Empty;
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"Result: {Operations.Add(firstNumber, secondNumber)}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Result: {Operations.Subtract(firstNumber, secondNumber)}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Result: {Operations.Multiply(firstNumber, secondNumber)}");
+                    break;
+                case "/":
+                    try
+                    {
+                        Console.WriteLine($"Result: {Operations.Divide(firstNumber, secondNumber)}");
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation selected.");
+                    break;
+            }
         }
-        catch (DivideByZeroException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-        break;
-    default:
-        Console.WriteLine("Invalid operation selected.");
-        break;
-}   
+    }
+}
